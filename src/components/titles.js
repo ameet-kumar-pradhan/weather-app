52a317da90d866eaadb778c54445b3ba01370bb2
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-22 16:59:22 +0530

    default

---
+++ src/components/titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = (props) => {
+   return (<div>
+      <h1>Weather App</h1>
+      <p>Get weather report from all around the globe</p>
+   </div>
+   )
+}
+export default Titles;
