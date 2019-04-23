6a6ee7d3e658ed8b71b1900043a727da3618e677
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-23 12:08:26 +0530

    Changes in form.js

--- src/components/form.js
+++ src/components/form.js
@@ -5,9 +5,8 @@ const Form = props => (
    <form onSubmit={props.loadWeather}>
       <input type="text" name="city" placeholder="City" />
       <input type="text" name="country" placeholder="Country" />
-      <button>
-         Get Weather
-      </button>
+      <button type="submit">Get Weather</button>
+      <button type="reset">Reset</button>
    </form>
 
 )
