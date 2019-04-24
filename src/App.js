f4a7a1c451c0d7ba15a99d21393836e4b10043d6
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-24 18:27:31 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ class App extends React.Component {
       })
     } else {
       this.setState({
+
         error: "Please input search values..."
       })
     }
--- src/components/form.js
+++ src/components/form.js
@@ -3,19 +3,22 @@ import '../assets/fomantic/dist/semantic.min.css';
 
 const Form = props => (
 
-   <form onSubmit={props.loadWeather} class="ui form">
-      <div class="ui input">
-         <input type="text" name="city" placeholder="City" />
-         <input type="text" name="country" placeholder="Country" />
-      </div>
+   <form onSubmit={props.loadWeather} class="ui">
       <div>
+         <div class="ui input">
+            <input type="text" name="city" placeholder="City" />
+            <input type="text" name="country" placeholder="Country" />
+         </div>
+
          <button class="ui primary button">
             Get Weather
       </button>
          <button class="ui button" type="reset">
             Reset
       </button>
+
       </div>
+
 
    </form >
 
